81b8d2badac33b857afb4481246deea79cc46f76
hakanu <hakanu@hakanu-macbookpro2.roam.corp.google.com>
2015-02-12 16:49:25 +0000

    Add more purecss.

--- static/js/angular-filters.js
+++ static/js/angular-filters.js
@@ -1,6 +1,5 @@
 angular.module('yt', []).filter('truncate', function() {
   return function(input) {
-    console.log('input: ' + input);
     if (input) {
       var startIndex = input.indexOf("v=") + 2;
       return input.substring(startIndex, input.length);
